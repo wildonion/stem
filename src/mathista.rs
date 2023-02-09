4605bf4030c4792218b02692fc353f4951702c25
dewo <marefimoghaddam@unixerr.com>
2023-02-09 09:11:37 +0100

    Update mathista.rs

--- src/mathista.rs
+++ src/mathista.rs
@@ -15,7 +15,7 @@
 // https://www.learnpytorch.io/
 // https://pytorch.org
 // https://github.com/LaurentMazare/tch-rs
-// https://nalgebra.org/
+// https://nalgebra.org/ => impl dl and ml algos
 // https://drive.google.com/drive/folders/1elbMIrg5_NlNMzrKAX_KNjJzgD-MvNQd
 // https://drive.google.com/file/d/1n_7VZtBk5Vy_RLyb2IXNtWKurZ9NSwyn/view?usp=sharing
 
