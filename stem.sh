b4655b3f9b483a92be2dfda56d64bfc8783fba7a
dewo <abarmardeatashyne@gmail.com>
2023-06-22 22:14:57 +0330

    walleXerr -> stem

--- stem.sh
+++ stem.sh
@@ -5,8 +5,8 @@ curl https://get.wasmer.io -sSfL | sh
 curl https://wasmtime.dev/install.sh -sSf | bash
 rustup target add wasm32-wasi # compilation target for wasm32 wasi WebAssembly
 # rustup target add wasm32-unknown-unknown # compilation target for browser-based WebAssembly
-cargo build --bin walleXerr --target wasm32-wasi --release
-sudo cp ../../../target/wasm32-wasi/release/walleXerr.wasm ./walleXerr.wasm
-wasm-opt -Oz walleXerr.wasm -o walleXerr.wasm # execute default optimization, passes, super-focusing on code
-wasmtime walleXerr.wasm
-wasmer run walleXerr.wasm 
+cargo build --bin stem --target wasm32-wasi --release 
+sudo cp ../../../target/wasm32-wasi/release/stem.wasm ./stem.wasm
+wasm-opt -Oz stem.wasm -o stem.wasm # execute default optimization, passes, super-focusing on code
+wasmtime stem.wasm
+wasmer run stem.wasm 
