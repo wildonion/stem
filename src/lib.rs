ed3d2d48beb1b55b0435a2c304f65b433d7c3dc4
dewo <abarmardeatashyne@gmail.com>
2023-08-05 14:33:35 +0330

    bpf refs

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,19 @@
 
 
 
-
-
-
+/*
+    with BPF VM we can compile the whole node 
+    into an .elf or .so which contains the 
+    BPF bytecode that can be executed from 
+    the linux kernel. LLVM13 is needed 
+    to compile BPF bytecode.
+*/
 pub async fn bpf_loader(){
 
-
+    // TODO - build macros inside the utils.rs 
+    // https://cheats.rs/#behind-the-scenes
+    // https://blog.redsift.com/labs/writing-bpf-code-in-rust/
+    // bpf loader
+    // ... 
     
 }
