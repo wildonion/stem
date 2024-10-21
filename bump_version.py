c0408dfa43b8fd3e891792114225f8e3fc933386
wildonion <ea_pain@yahoo.com>
2024-10-21 19:23:12 +0330

    feat: bump the stemlib also

--- bump_version.py
+++ bump_version.py
@@ -9,6 +9,7 @@ if len(sys.argv) < 2:
 # Get the new version from the command line arguments
 new_version = sys.argv[1]
 
+# ---------- update the main Cargo
 # Read the Cargo.toml file
 with open("Cargo.toml", "r") as file:
     lines = file.readlines()
@@ -25,6 +26,28 @@ with open("Cargo.toml", "w") as file:
 
 # Log the update process
 print(f"Updated Cargo.toml to version {new_version}")
+# --------------------------------------
+
+
+# ---------- update the stemlib Cargo
+
+# Read the Cargo.toml file
+with open("stemlib/Cargo.toml", "r") as file:
+    lines = file.readlines()
+
+# Update the version in the Cargo.toml file
+for i in range(len(lines)):
+    if lines[i].startswith("version ="):
+        lines[i] = f'version = "{new_version}"\n'  # Update the line with the new version
+        break  # Exit the loop once the version is updated
+
+# Write the updated content back to Cargo.toml
+with open("stemlib/Cargo.toml", "w") as file:
+    file.writelines(lines)
+
+# Log the update process
+print(f"Updated stemlib/Cargo.toml to version {new_version}")
+# --------------------------------------
 
 # Update the version in Dockerfile
 with open("Dockerfile", "r") as file:
